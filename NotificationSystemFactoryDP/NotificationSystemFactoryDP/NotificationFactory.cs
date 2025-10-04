f881abe81aa1d2cc5590417155a2398531d750b4
KiriakiKad <kiriakikadrefi@gmail.com>
2025-10-04 15:57:15 +0300

    Add NotificationSystemDP factory pattern

---
+++ NotificationSystemFactoryDP/NotificationSystemFactoryDP/NotificationFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationSystemFactoryDP
+{
+    public class NotificationFactory
+    {
+        public INotification SendNotification(string selectedType)
+        {
+
+            if (selectedType.Equals("sms")){
+                return new SmsNotification();
+            }else if (selectedType.Equals("email"))
+            {
+                return new EmailNotification();
+            }else if (selectedType.Equals("push"))
+            {
+                return new PushNotification();
+            }
+            else if (selectedType.Equals("quit"))
+            {
+                Environment.Exit(0);
+                return null;
+            }
+            else
+            {
+                throw new ArgumentException("Unsupported type");
+            }
+
+        }
+    }
+}
---
+++ NotificationSystemFactoryDP/NotificationSystemFactoryDP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationSystemFactoryDP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var factory = new NotificationFactory();
+
+            while (true)
+            {
+                Console.Write("Select the type of message that you wnat o send: (email, sms, push) or quit: ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) continue;
+
+                input = input.Trim().ToLower();
+
+                var notification = factory.SendNotification(input);
+
+                if (notification != null)
+                {
+                    Console.Write("Enter your message: ");
+                    var message = Console.ReadLine();
+                    notification.Send(message);
+                }
+            }
+        }
+    }
+}
